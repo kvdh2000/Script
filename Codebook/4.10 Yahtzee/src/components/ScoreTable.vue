e483ab43d84949b4b8ef5ab188f6ecd67b589a99
kvdh2000 <kishanvdh@gmail.com>
2026-06-16 16:00:17 +0200

    Yahtzee V3

--- Codebook/4.10 Yahtzee/src/components/ScoreTable.vue
+++ Codebook/4.10 Yahtzee/src/components/ScoreTable.vue
@@ -3,10 +3,6 @@
 
 	const dice = defineModel()
 
-	const total = computed(() =>
-  	dice.value.reduce((sum, die) => sum + die, 0)
-	)
-
 	const counts = computed(() => {
 		const frequency = {}
 
@@ -16,37 +12,15 @@
 
 		return frequency
 	})
-	
-	const ones = computed(() =>
-  	dice.value.filter(die => die === 1).length * 1
-	)
-
-	const twos = computed(() =>
-		dice.value.filter(die => die === 2).length * 2
-	)
-
-	const threes = computed(() =>
-		dice.value.filter(die => die === 3).length * 3
-	)
-
-	const fours = computed(() =>
-		dice.value.filter(die => die === 4).length * 4
-	)
-
-	const fives = computed(() =>
-		dice.value.filter(die => die === 5).length * 5
-	)
-
-	const sixes = computed(() =>
-		dice.value.filter(die => die === 6).length * 6
-	)
 
 	function dieTypes(dieType)
 	{
+		if(dieType === 0) return dice.value.reduce((sum, die) => sum + die, 0) 
 		return dice.value.filter(die => die == dieType).length * dieType
 	}
 
-	function hasCount(amount) {
+	function hasCount(amount) 
+	{
 		return Object.values(counts.value).includes(amount)
 	}
 	
@@ -62,11 +36,6 @@
 
 		return 0
 	}
-	
-	function getSorted()
-	{
-		return [...new Set(dice.value)].sort().join('')
-	}
 
 	const threeKind = computed(() => ofKind(3))
 
@@ -76,40 +45,40 @@
 		hasCount(3) && hasCount(2) ? 25 : 0
 	)
 	
-	const smallStraight = computed(() => {
-		const sorted = getSorted()
-
-		if
-		(
-			sorted.includes('1234') ||
-			sorted.includes('2345') ||
-			sorted.includes('3456')
-		) 
-		{
-			return 30
-		}
-		
-		return 0
-	})
-		
-	const largeStraight = computed(() =>
+	function straight(n)
 	{
-		const sorted = getSorted()
+		const sorted = [...new Set(dice.value)].sort((a, b) => a - b)
 
-		if
-		(
-			sorted === '12345' ||
-			sorted === '23456')
+		let streak = 1
+
+		for (let i = 1; i < sorted.length; i++)
 		{
-			return 40
+			if (sorted[i] === sorted[i - 1] + 1)
+			{
+				streak++
+
+				if (streak >= n)
+				{
+					return true
+				}
+			}
+			else
+			{
+				streak = 1
+			}
 		}
 
-		return 0
-	})
+		return false
+	}
+
+
+	const smallStraight = computed(() => straight(4) ? 30 : 0)
+
+	const largeStraight = computed(() => straight(5) ? 40 : 0)
 
 	const yahtzee = computed(() => hasCount(5) ? 50 : 0)
 
-	const upperScores = computed(() => ones.value + twos.value + threes.value + fours.value + fives.value + sixes.value)
+	const upperScores = computed(() => dieTypes(0))
 	const lowerScores = computed(() => threeKind.value+fourKind.value+smallStraight.value+largeStraight.value+yahtzee.value)	
 </script>
 
